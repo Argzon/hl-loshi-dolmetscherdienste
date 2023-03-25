05f3eb585da30a728bc8590a415627deb6edc63d
Argzon <argzon01@gmail.com>
2023-03-25 02:20:53 +0100

    created uber uns page

---
+++ src/pages/uber-uns.js
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export default function UberUns() {
+  return (
+    <>
+      <div className="container">
+        <Navbar />
+      </div>
+      <Footer />
+    </>
+  );
+}
